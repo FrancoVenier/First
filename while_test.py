83cea10e55c12ee2ac8c5588be6003d3041d8d1f
FrancoVenier <francovenier00@gmail.com>
2021-07-27 01:45:22 -0300

    Poke_fight_test_1

---
+++ while_test.py
@@ -0,0 +1,13 @@
+
+first_number = 11
+
+while first_number > 0:
+    first_number -= 1
+    print(first_number)
+    if first_number % 2 == 0:
+        print("este numero es par")
+    else:
+        print("este numero es impar")
+
+print("finish task")
+
