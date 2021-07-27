2827edbb3d0c3be12b24bdd182312c1fadefcd5c
FrancoVenier <francovenier00@gmail.com>
2021-07-26 21:54:00 -0300

    add_icecream_test_part_2

--- eat_icecream.py
+++ eat_icecream.py
@@ -1,11 +1,19 @@
-tengo_ganas_de_helado_input = input("¿Queres un Helado? (Si / No): ")
-tiene_dinero_input = input("¿Tienes dinero para un helado? (Si / No): ")
-esta_abierto_el_local_input = input("¿Esta abierto el local? (Si / No): ")
-esta_tu_hermana_para_pagar_input = input("¿Estas con tu hermana asi lo paga? (Si / No): ")
+tengo_ganas_de_helado_input = input("¿Queres un Helado? (Si / No): ") .upper()
 
-sale_helado = tengo_ganas_de_helado_input == "Si"
-puede_pagarlo = tiene_dinero_input == "Si" or esta_tu_hermana_para_pagar_input == "Si"
-esta_abierto_el_local = esta_abierto_el_local_input == "Si"
+if tengo_ganas_de_helado_input == "SI":
+    sale_helado = True
+elif tengo_ganas_de_helado_input == "NO":
+    sale_helado = False
+else:
+    print("Te he dicho que me digas si o no, no se que has dicho pero cuento como que no")
+    sale_helado = False
+
+tiene_dinero_input = input("¿Tienes dinero para un helado? (Si / No): ") .upper()
+esta_abierto_el_local_input = input("¿Esta abierto el local? (Si / No): ") .upper()
+esta_tu_hermana_para_pagar_input = input("¿Estas con tu hermana asi lo paga? (Si / No): ") .upper()
+
+puede_pagarlo = tiene_dinero_input == "SI" or esta_tu_hermana_para_pagar_input == "SI"
+esta_abierto_el_local = esta_abierto_el_local_input == "SI"
 
 if sale_helado and puede_pagarlo and esta_abierto_el_local:
     print("Sale Helado perri :) ")
